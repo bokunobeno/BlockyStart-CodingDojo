c53bb61f28b3647a8fffbd2f686475e250f443ec
benobun <bokunobeno@gmail.com>
2022-07-31 16:56:57 +0300

    added version 0

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+	[SerializeField] private string _tagID = "Player";
+	[SerializeField] private string _sceneName;
+
+
+	private void OnTriggerEnter(Collider _collider){
+		if(_collider.gameObject.CompareTag(_tagID)){
+			SceneManager.LoadScene(_sceneName);
+		}
+	}
+}
